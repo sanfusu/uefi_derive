9af4981f1f65eeec9b873f4739c880ef2c6e2adb
sanfusu <sanfusu@foxmail.com>
2021-01-13 00:26:34 +0800

    remove the unused use

--- src/revision.rs
+++ src/revision.rs
@@ -1,9 +1,6 @@
 use std::str::FromStr;
-
 use proc_macro2::TokenStream;
 use uefi_raw::system_table::Revision;
-use version::Version;
-
 pub(crate) fn revision_proc(item: TokenStream) -> TokenStream {
     let rev_str = item.to_string();
     let revision: Revision = Revision::from_str(&rev_str).unwrap();
